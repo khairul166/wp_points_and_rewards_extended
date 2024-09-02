76954842e982901238172e248f9ebc32206e9bb2
khairul166 <khirulislam166@gmail.com>
2024-09-03 02:19:29 +0600

    03.09.24 2.19am

--- js/custom-script.js
+++ js/custom-script.js
@@ -12,8 +12,10 @@ jQuery(document).ready(function ($) {
             points: totalPointsToApply, // Send the total points to be applied
             update_cart_only: true // Flag to indicate cart update without deduction
         };
+
         $('#apply-points-spinner').removeClass('hidden');
         $('#overlay').show();
+
         // Send the AJAX request
         $.post(custom_script_params.ajax_url, data, function (response) {
             $('#apply-points-spinner').addClass('hidden');
@@ -27,9 +29,10 @@ jQuery(document).ready(function ($) {
                 var pointsEarned = response.points_earned;
 
                 // Update the cart totals and discount amount
-                $('.fee td').html('-' + discountAmount);
+                $('.fee td').html('-' + discountAmount + ' <a href="#" class="remove-points">[remove]</a>');
                 $('.order-total td').html(response.total_amount);
                 $('.points-earned td').html(pointsEarned + ' Points');
+
 
                 if (response.points_redemption_amount === 0) {
                     $('.fee').hide();
@@ -57,6 +60,56 @@ jQuery(document).ready(function ($) {
         });
     }
 
+   // Function to remove points redemption
+function removePointsRedemption() {
+    totalPointsApplied = 0; // Reset the applied points
+    localStorage.removeItem('totalPointsApplied'); // Clear the localStorage
+
+    var data = {
+        action: 'apply_points_redemption',
+        nonce: custom_script_params.nonce,
+        points: 0, // Reset points on the server
+        update_cart_only: true // Flag to indicate cart update without deduction
+    };
+
+    $('#apply-points-spinner').removeClass('hidden');
+    $('#overlay').show();
+
+    // Send the AJAX request to remove points
+    $.post(custom_script_params.ajax_url, data, function (response) {
+        $('#apply-points-spinner').addClass('hidden');
+        $('#overlay').hide();
+        if (response.success) {
+            // Update the cart totals and discount amount
+            $('.fee td').html('-' + response.discount_amount);
+            $('.order-total td').html(response.total_amount);
+            $('.points-earned td').html(response.points_earned + ' Points');
+
+            
+            var discount_amt=document.querySelector('tr.fee');
+            var disamt= discount_amt.querySelector('.woocommerce-Price-amount').textContent.replace(/[^\d.]/g, '');
+            var finaldisamt = parseFloat(disamt.replace(',', ''));
+
+
+            console.log(finaldisamt);
+            // Check if the discount amount is 0, and hide the Points Redemption row if it is
+            if (finaldisamt === 0) {
+                $('.fee').hide();
+            }
+
+            $('.woocommerce-message, .woocommerce-error').remove();
+            $('.woocommerce-cart-form').before('<div class="woocommerce-message" role="alert">Points removed.</div>');
+
+            $('body').trigger('update_checkout');
+        }
+    }).fail(function () {
+        $('#apply-points-spinner').addClass('hidden');
+        $('#overlay').hide();
+        alert('Error processing the request.');
+    });
+}
+
+
     // Event listener for the "Apply Points" button
     $('#apply_points_btn').on('click', function () {
         var points = parseFloat($('#points_redemption').val());
@@ -75,6 +128,12 @@ jQuery(document).ready(function ($) {
         }
     });
 
+    // Event listener for the "Remove Points" link
+    $(document).on('click', '.remove-points', function (e) {
+        e.preventDefault();
+        removePointsRedemption();
+    });
+
     // Reset the points after checkout is completed
     $(document.body).on('checkout_order_processed', function () {
         localStorage.removeItem('totalPointsApplied');
@@ -84,17 +143,15 @@ jQuery(document).ready(function ($) {
     $('.woocommerce-cart').on('click', '.checkout-button', function () {
         localStorage.removeItem('totalPointsApplied');
     });
-});
 
-jQuery(document).ready(function ($) {
     var feeRow = document.querySelector('tr.fee');
-    if(feeRow){
-      var feeamttext = feeRow.querySelector('.woocommerce-Price-amount').textContent.replace(/[^\d.]/g, '');
-    
-      pointsRedemptionAmountElement =  parseFloat(feeamttext.replace(',', ''));
-      
-         if (isNaN(pointsRedemptionAmountElement) || pointsRedemptionAmountElement === 0) {
-              $('.fee').hide();
-          }
+    if (feeRow) {
+        var feeamttext = feeRow.querySelector('.woocommerce-Price-amount').textContent.replace(/[^\d.]/g, '');
+
+        pointsRedemptionAmountElement = parseFloat(feeamttext.replace(',', ''));
+
+        if (isNaN(pointsRedemptionAmountElement) || pointsRedemptionAmountElement === 0) {
+            $('.fee').hide();
+        }
     }
-  });
+});
